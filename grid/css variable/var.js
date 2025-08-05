8c2e8870dd811db31cdec39d48e9f6887d08f7a6
ajay-mh-12 <ajaymh02@gmail.com>
2025-08-05 17:11:52 +0530

    practiced

--- grid/css variable/var.js
+++
@@ -1,9 +0,0 @@
-var swatches = document.querySelectorAll('.swatches span');
-var root = document.querySelector(':root')
-
-
-swatches.forEach((swatch)
-swatch.addEvenListener('click',(a)
-root.Style.setProperty('--theme-color',e.target.style.background);
-})
-})
